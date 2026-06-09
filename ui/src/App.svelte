f3b14da8dc75540dc2ec9e0446c5b0dd3f0c40e0
Anton Keks <anton@azib.net>
2026-06-10 00:10:08 +0300

    add sample menu and NotFoundPage

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,16 +1,33 @@
 <script lang="ts">
   import {t} from 'i18n'
   import Toasts from 'src/components/Toasts.svelte'
-  import {Route, Router} from '@keksworks/svelte-tiny-router'
+  import {activePath, Link, Route, Router} from '@keksworks/svelte-tiny-router'
   import HomePage from 'src/pages/HomePage.svelte'
+  import NotFoundPage from 'src/pages/NotFoundPage.svelte'
+
+  const pages = [
+    {path: '/', title: t.home.title, component: HomePage},
+  ]
+
+  $: activePage = pages.find(p => p.path === $activePath)
 </script>
 
 <svelte:head>
-  <title>{t.title}</title>
+  <title>{activePage?.title} - {t.title}</title>
 </svelte:head>
 
 <Toasts/>
 
-<Router>
- <Route path="/" component={HomePage}/>
-</Router>
+<menu class="flex gap-4 p-4 bg-gray-100">
+  <span class="font-bold">LOGO</span>
+  {#each pages as page}
+    <Link to={page.path} label={page.title} class="{page === activePage ? 'text-underline!' : ''}"/>
+  {/each}
+</menu>
+
+<main class="p-4">
+  <h1 class="mb-4">{activePage?.title}</h1>
+  <Router routes={pages}>
+    <Route component={NotFoundPage}/>
+  </Router>
+</main>
--- ui/src/pages/HomePage.svelte
+++ ui/src/pages/HomePage.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
-  import {t} from 'i18n'
 </script>
 
-<main class="text-center pt-10">
-  <h1>{t.title}</h1>
-</main>
+<p class="text-center pt-10">
+  Welcome
+</p>
