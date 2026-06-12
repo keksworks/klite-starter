02d7c2ec9129263e37f29014d96cf9bfbfba80c9
Anton Keks <anton@azib.net>
2026-06-12 12:57:53 +0300

    update router

--- ui/src/App.svelte
+++ ui/src/App.svelte
@@ -1,33 +1,34 @@
 <script lang="ts">
   import {t} from 'i18n'
   import Toasts from 'src/components/Toasts.svelte'
-  import {activePath, Link, Route, Router} from '@keksworks/svelte-tiny-router'
+  import {Link, Route, Router} from '@keksworks/svelte-tiny-router'
   import HomePage from 'src/pages/HomePage.svelte'
   import NotFoundPage from 'src/pages/NotFoundPage.svelte'
 
-  const pages = [
-    {path: '/', title: t.home.title, component: HomePage},
-  ]
+  const pages = {
+    '/': {title: t.home.title, component: HomePage},
+  }
 
-  $: activePage = pages.find(p => p.path === $activePath)
+  let matchedPath: keyof typeof pages
+  $: activePage = pages[matchedPath]
 </script>
 
 <svelte:head>
-  <title>{activePage?.title} - {t.title}</title>
+  <title>{activePage ? activePage.title + ' - ' : ''}{t.title}</title>
 </svelte:head>
 
 <Toasts/>
 
 <menu class="flex gap-4 p-4 bg-gray-100">
   <span class="font-bold">LOGO</span>
-  {#each pages as page}
-    <Link to={page.path} label={page.title} class="{page === activePage ? 'text-underline!' : ''}"/>
+  {#each Object.entries(pages) as [path, page]}
+    <Link to={path} label={page.title} class="{path === matchedPath ? 'font-bold' : ''}"/>
   {/each}
 </menu>
 
 <main class="p-4">
   <h1 class="mb-4">{activePage?.title}</h1>
-  <Router routes={pages}>
+  <Router routes={pages} bind:matchedPath>
     <Route component={NotFoundPage}/>
   </Router>
 </main>
