b3050ecc2dd139aa311880ccb10de6a0c135f06a
Anton Keks <anton@azib.net>
2026-09-07 21:05:39 +0300

    reset updatedAt at the end of each test transaction, so that next transaction can use same entities again

--- test/db/DBTest.kt
+++ test/db/DBTest.kt
@@ -4,10 +4,13 @@ import db.TestData.user
 import klite.Config
 import klite.isTest
 import klite.jdbc.DBMigrator
+import klite.jdbc.UpdatableEntity
 import klite.jdbc.exec
 import klite.jdbc.useAppDBUser
+import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import users.UserRepository
+import kotlin.reflect.full.memberProperties
 
 abstract class DBTest: klite.jdbc.DBTest() {
   companion object {
@@ -18,12 +21,19 @@ abstract class DBTest: klite.jdbc.DBTest() {
         migrate()
       }
       useAppDBUser()
-      UserRepository(db).save(user)
     }
   }
 
   @BeforeEach override fun startTx() {
     super.startTx()
+    UserRepository(db).save(user)
     db.exec("call set_app_user(${user.id.value})") {}
+  }
+
+  @AfterEach fun resetTestData() {
+    TestData::class.memberProperties.forEach {
+      val entity = it.get(TestData)
+      if (entity is UpdatableEntity) entity.updatedAt = null
+    }
   }
 }
