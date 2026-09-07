85f201035c89fb84362ddd0652e7d6c97328b970
Anton Keks <anton@azib.net>
2026-09-07 19:36:21 +0300

    add an icon example

--- ui/src/pages/HomePage.svelte
+++ ui/src/pages/HomePage.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
   import {t} from 'i18n'
+  import Icon from 'src/icons/Icon.svelte'
 </script>
 
 <p class="text-center pt-10">
   {t.home.welcome}
+  <Icon name="circle-x"/>
 </p>
